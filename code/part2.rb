c05c097c45b817d88090af4d6deaa5bb9e66d37b
Andrew K <akephart@colgate.edu>
2013-02-10 18:50:00 -0500

    part2

--- code/part2.rb
+++ code/part2.rb
@@ -1,14 +1,36 @@
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
-def rps_result(m1, m2)
-  # YOUR CODE HERE
+def rps_result(s1, s2)
+    results = { ['r','r']=>1,
+                ['r','p']=>2,
+                ['r','s']=>1,
+                ['p','r']=>1,
+                ['p','p']=>1,
+                ['p','s']=>2,
+                ['s','r']=>2,
+                ['s','p']=>1,
+                ['s','s']=>1,
+    }
+    results[[s1,s2]]
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+    raise WrongNumberOfPlayersError unless game.length ==2
+    s1,s2=game[0][1],game[1][1]
+    s1.downcase!
+    s2.downcase!
+    raise NoSuchStrategyError unless (s1 =~/^[rps]$/ and s2 =~ /^[rps]$/)
+    if rps_result(s1,s2) == 1 then return game[0]
+    else
+        return game[1]
+    end
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+     if tournament[0][0].class == 'a'.class
+        rps_game_winner tournament 
+     else
+        rps_game_winner [(rps_tournament_winner tournament[0]),(rps_tournament_winner tournament[1])]
+    end
 end
