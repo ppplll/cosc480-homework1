8c5d72e6b8fb70d78c504bfc7ed313c47802a467
Andrew K <akephart@colgate.edu>
2013-02-10 17:39:30 -0500

    part1

--- code/part1.rb
+++ code/part1.rb
@@ -1,7 +1,17 @@
 def palindrome?(str)
-  # YOUR CODE HERE
+    str=str.upcase.gsub(/\W/,'')
+    str==str.reverse
 end
 
 def count_words(str)
-  # YOUR CODE HERE
+    h=Hash.new
+    str=str.downcase
+    str.split(/\W+/).each do |word|
+        if h.has_key? word  
+            h[word]+=1 
+        else
+            h[word]=1
+        end
+    end
+    return h
 end
