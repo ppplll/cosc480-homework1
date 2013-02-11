fcccd2e4c1a9d5f39081bfd91bbc5bf37dd806c9
Andrew K <akephart@colgate.edu>
2013-02-10 20:55:57 -0500

    part3

--- code/part3.rb
+++ code/part3.rb
@@ -1,3 +1,21 @@
 def combine_anagrams(words)
- # YOUR CODE HERE
+    out=[]
+    words.each do |word|
+        sword = sort_word word
+        added = false
+        for i in 0..(out.length-1)
+            if sort_word(out[i][0]) == sword
+                out[i]<<word 
+                added = true
+            end
+        end
+        out<<[word] if added == false
+    end
+    return out
 end
+
+def sort_word(word)
+    word.downcase.chars.sort.join
+end
+
+
